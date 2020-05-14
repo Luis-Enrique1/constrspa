3a2b5fa97a6c60c93ba8e3ad59bb3b6b60004fd4
Electricidad <piowanau@gmail.com>
2020-05-14 12:02:27 -0400

    service worker

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
-import Contact from './pages/Contact.jsx';
-import About from './pages/About.jsx';
 import './App.css';
 import 'materialize-css/dist/css/materialize.css'
+
+const Contact = React.lazy(() => import('./pages/Contact'));
+const About = React.lazy(() => import('./pages/About'));
 
 function App() {
   return (
     <Router>
-    <div>
-      <Route exact path="/" component={Home}/>
-      <Route path="/contact" component={Contact}/>
-      <Route path="/about" component={About}/>
-    </div>
+      <div>
+        <Suspense fallback={<div className="progress"><div className="indeterminate"></div></div>}>
+          <Route exact path="/" component={Home}/>
+          <Route path="/contact" component={Contact}/>
+          <Route path="/about" component={About}/>
+        </Suspense>
+      </div>
   </Router>
   );
 }
